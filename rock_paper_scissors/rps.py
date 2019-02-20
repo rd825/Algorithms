897cc7355850b29ba2973dbc97b7f4288af5e1c8
Rahul Desai <rd825@georgetown.edu>
2019-02-20 16:28:04 -0500

    Finished rps.py -- it was my peer whiteboarding problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,24 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    options = ['rock', 'paper', 'scissors']  # the options each player has
+    plays = []  # the results of each round
+
+    def find_outcome(rounds, result=[]):
+        if rounds == 0:
+            plays.append(result)
+            return
+        for option in options:
+            find_outcome(rounds - 1, result + [option])
+    find_outcome(n, [])
+    return plays
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
