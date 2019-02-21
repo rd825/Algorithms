1ca448dff0b0bb6ba71224a9a734a515a131672f
Rahul Desai <rd825@georgetown.edu>
2019-02-21 18:20:20 -0500

    attempted making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,28 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    if amount < 0 or len(denominations) == 0:
+        return 0
+    elif amount == 0:
+        return 1
+    else:
+        ways = 0
+        for i in range(len(denominations)):
+            ways += making_change(amount - denominations[i], denominations)
+        return ways
 
 
-if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+print(making_change(10, [1, 5, 10, 25, 50]))
+
+# if __name__ == "__main__":
+#     # Test our your implementation from the command line
+#     # with `python making_change.py [amount]` with different amounts
+#     if len(sys.argv) > 1:
+#         denominations = [1, 5, 10, 25, 50]
+#         amount = int(sys.argv[1])
+#         print("There are {ways} ways to make {amount} cents.".format(
+#             ways=making_change(amount, denominations), amount=amount))
+#     else:
+#         print("Usage: making_change.py [amount]")
